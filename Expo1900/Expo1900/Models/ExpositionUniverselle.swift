17c91f8315734e44ddb0814bf362ebdebfbd3d2d
volgaHwang <wltkak2@gmail.com>
2023-01-09 17:09:31 +0900

    feat: ExpositionUniverselle 모델 구현 반영

---
+++ Expo1900/Expo1900/Models/ExpositionUniverselle.swift
@@ -0,0 +1,16 @@
+//
+//  ExpositionUniverselle.swift
+//  Expo1900
+//
+//  Created by kakao on 2023/01/09.
+//
+
+import Foundation
+
+struct ExpositionUniverselle: Decodable {
+    let title: String
+    let visitors: Int
+    let location: String
+    let duration: String
+    let description: String
+}
