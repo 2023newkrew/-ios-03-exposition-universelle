c775cb3552a11f2697405c9d6d9ee6f931362d2d
kakao <kakao@summer-love-pc.local>
2023-01-10 12:53:03 +0900

    feat: mainViewController 구현

--- Expo1900/Expo1900/MainViewController.swift
+++ Expo1900/Expo1900/MainViewController.swift
@@ -7,12 +7,68 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    enum Text {
+        static let visitor = "방문객 : "
+        static let place = "개최지 : "
+        static let period = "개최기간 : "
+        static let seeEntries = "한국의 출품작 보러가기"
+        
+        static let poster = "poster"
+        static let flag = "flag"
+        static let mainAssetName = "exposition_universelle_1900"
+    }
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleImageView: UIImageView!
+    @IBOutlet weak var visitorLabel: UILabel!
+    @IBOutlet weak var placeLabel: UILabel!
+    @IBOutlet weak var periodLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    @IBOutlet weak var leftFlageImageView: UIImageView!
+    @IBOutlet weak var seeEntriesButton: UIButton!
+    @IBOutlet weak var rightFlagImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.configure()
     }
-
-
 }
 
+extension MainViewController {
+    func configure() {
+        let expositionIntroduction = fetchExpositionIntroductionData()
+        switch expositionIntroduction {
+        case .success(let result):
+            configureObjects(expositionIntoroduction: result)
+        case .failure(let error):
+            print(error)
+        }
+    }
+    
+    func configureObjects(expositionIntoroduction: ExpositionIntroduction) {
+        self.titleLabel.text = expositionIntoroduction.title
+        self.titleImageView.image = UIImage(named: Text.poster)
+        self.visitorLabel.text = Text.visitor + String(expositionIntoroduction.visitors)
+        self.placeLabel.text = Text.place +  expositionIntoroduction.location
+        self.periodLabel.text = Text.period + expositionIntoroduction.duration
+        self.descriptionTextView.text = expositionIntoroduction.description
+        
+        self.leftFlageImageView.image = UIImage(named: Text.flag)
+        self.seeEntriesButton.setTitle(Text.seeEntries, for: .normal)
+        self.rightFlagImageView.image = UIImage(named: Text.flag)
+    }
+}
+
+extension MainViewController {
+    func fetchExpositionIntroductionData() -> Result<ExpositionIntroduction, DecodingError> {
+        guard let data = NSDataAsset(name: Text.mainAssetName)?.data else {
+            return .failure(.dataAssetConvertingFailure)
+        }
+        guard let product = try? JSONDecoder().decode(ExpositionIntroduction.self, from: data) else {
+            return .failure(.decodingFailure)
+        }
+        return .success(product)
+    }
+}
