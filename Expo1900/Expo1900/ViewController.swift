bf3c902c914528e01203dcc8d779cb2b55fff6be
volgaHwang <wltkak2@gmail.com>
2023-01-10 10:09:41 +0900

    feat: ExpoViewController에 Subviews 추가
    - 스크롤을 위해 ScrollView를 추가했고, 그 위에 StackView를 추가했습니다.
    - 렌더링에 필요한 Label들과 UIImageView를 추가했습니다.
    - layout을 추가 반영했습니다.

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -7,12 +7,71 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return scrollView
+    }()
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 15
+        return stackView
+    }()
+    
+    private lazy var titleLabel = UILabel()
+    private lazy var imageView = UIImageView()
+    private lazy var visitorLabel = UILabel()
+    private lazy var locationLabel = UILabel()
+    private lazy var durationLabel = UILabel()
+    private lazy var descriptionLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setHierarchy()
+        setLayout()
+        setAttribute()
     }
-
-
+    
+    private func setHierarchy() {
+        view.addSubview(scrollView)
+        
+        scrollView.addSubview(stackView)
+        
+        [titleLabel, imageView, visitorLabel,
+         locationLabel, durationLabel, descriptionLabel].forEach { subView in
+            subView.translatesAutoresizingMaskIntoConstraints = false
+            stackView.addArrangedSubview(subView)
+        }
+    }
+    
+    private func setLayout() {
+        stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 14).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -14).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        stackView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 20).isActive = true
+        stackView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -28).isActive = true
+        
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        imageView.widthAnchor.constraint(equalTo: stackView.widthAnchor, multiplier: 0.4).isActive = true
+    }
+    
+    private func setAttribute() {
+        titleLabel.adjustsFontForContentSizeCategory = true
+        titleLabel.font = UIFont.preferredFont(forTextStyle: .title1)
+        titleLabel.numberOfLines = 0
+        titleLabel.textAlignment = .center
+        
+        imageView.contentMode = .scaleAspectFit
+        
+        descriptionLabel.numberOfLines = 0
+    }
 }
 
