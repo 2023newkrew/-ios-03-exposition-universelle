d82277e2a2c315f2db7127529996ccc1aae74175
forest.jay <forestsj30@gmail.com>
2023-01-10 17:08:48 +0900

    feat: EntryTableViewCell 구현

---
+++ Expo1900/Expo1900/EntryTableViewCell.swift
@@ -0,0 +1,78 @@
+//
+//  EntryTableViewCell.swift
+//  Expo1900
+//
+//  Created by kakao on 2023/01/10.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+    
+    private let entryImageView: UIImageView = UIImageView(frame: .zero)
+    private let descriptionVerticalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.distribution = .fill
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        return stackView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.adjustsFontForContentSizeCategory = true
+        label.font = .preferredFont(forTextStyle: .title1)
+        return label
+    }()
+    
+    private let shortDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.adjustsFontForContentSizeCategory = true
+        label.font = .preferredFont(forTextStyle: .body)
+        return label
+
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier reuseIdentifer: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifer)
+        self.configureHierarchy()
+        self.configureConstraints()
+        self.accessoryType = .checkmark
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    func configureCell(with item: ExpositionItem) {
+        self.entryImageView.image = UIImage(named: "\(item.imageName)")
+        self.titleLabel.text = item.name
+        self.shortDescriptionLabel.text = item.shortDescription
+    }
+    
+    private func configureHierarchy() {
+        self.contentView.addSubview(self.entryImageView)
+        self.contentView.addSubview(self.descriptionVerticalStackView)
+ 
+        self.descriptionVerticalStackView.addArrangedSubview(self.titleLabel)
+        self.descriptionVerticalStackView.addArrangedSubview(self.shortDescriptionLabel)
+    }
+    
+    private func configureConstraints() {
+        self.entryImageView.translatesAutoresizingMaskIntoConstraints = false
+        self.descriptionVerticalStackView.translatesAutoresizingMaskIntoConstraints = false
+        self.titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.shortDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            self.entryImageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 10),
+            self.entryImageView.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor),
+            self.entryImageView.widthAnchor.constraint(equalToConstant: 50),
+            self.entryImageView.heightAnchor.constraint(equalTo: self.entryImageView.widthAnchor),
+            self.descriptionVerticalStackView.leadingAnchor.constraint(equalTo: self.entryImageView.trailingAnchor),
+            self.descriptionVerticalStackView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            self.descriptionVerticalStackView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
+            self.descriptionVerticalStackView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor)
+        ])
+    }
+}
