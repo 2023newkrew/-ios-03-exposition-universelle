8601fcb2211e686c49950c982fe6117395124b5f
forest.jay <forestsj30@gmail.com>
2023-01-12 10:38:50 +0900

    refactor: ExpositionService 객체 구현
    - 데이터 fetch 로직 별도 타입으로 분리

---
+++ Expo1900/Expo1900/Service/ExpositionService.swift
@@ -0,0 +1,39 @@
+//
+//  ExpositionService.swift
+//  Expo1900
+//
+//  Created by kakao on 2023/01/12.
+//
+
+import UIKit
+
+class ExpositionService {
+    private enum Text {
+        static let mainAssetName = "exposition_universelle_1900"
+        static let itemsAssetName = "items"
+    }
+    
+    static let shared = ExpositionService()
+    
+    private init() { }
+
+    func fetchExpositionIntroductionData() -> Result<ExpositionIntroduction, DecodingError> {
+        guard let data = NSDataAsset(name: Text.mainAssetName)?.data else {
+            return .failure(.dataAssetConvertingFailure)
+        }
+        guard let product = try? JSONDecoder().decode(ExpositionIntroduction.self, from: data) else {
+            return .failure(.decodingFailure)
+        }
+        return .success(product)
+    }
+    
+    func fetchExpositionItemsData() -> Result<[ExpositionItem], DecodingError> {
+        guard let data = NSDataAsset(name: Text.itemsAssetName)?.data else {
+            return .failure(.dataAssetConvertingFailure)
+        }
+        guard let product = try? JSONDecoder().decode([ExpositionItem].self, from: data) else {
+            return .failure(.decodingFailure)
+        }
+        return .success(product)
+    }
+}
