e30aa78db8113b9fc6ac6a966b83e0e3d5db3b8d
john.lim <limjae96@gmail.com>
2023-01-12 16:18:17 +0900

    feat: 상세 화면 가로모드 시 이미지 표시 변경 기능 추가

--- Expo1900/Expo1900/ViewControllers/ExpositionDetailsViewController.swift
+++ Expo1900/Expo1900/ViewControllers/ExpositionDetailsViewController.swift
@@ -50,12 +50,34 @@ class ExpositionDetailsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setHierarchy()
-        setLayout()
         setAttribute()
     }
     
-    private func setHierarchy() {
+    override func viewWillLayoutSubviews() {
+        clearHierarchy()
+        switch UIScreen.main.bounds.size.width > UIScreen.main.bounds.size.height {
+        case true:
+            setHierarchyForLandscape()
+            setLayoutForLandscape()
+        case false:
+            setHierarchyForPortrait()
+            setLayoutForPortrait()
+        }
+    }
+    
+    private func clearHierarchy() {
+        view.subviews.forEach { subview in
+            subview.removeFromSuperview()
+        }
+        scrollView.subviews.forEach { subview in
+            subview.removeFromSuperview()
+        }
+        stackView.subviews.forEach { subview in
+            subview.removeFromSuperview()
+        }
+    }
+    
+    private func setHierarchyForPortrait() {
         view.addSubview(scrollView)
         
         scrollView.addSubview(stackView)
@@ -67,7 +89,18 @@ class ExpositionDetailsViewController: UIViewController {
             }
     }
     
-    private func setLayout() {
+    private func setHierarchyForLandscape() {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        
+        view.addSubview(scrollView)
+        scrollView.addSubview(stackView)
+        
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        stackView.addArrangedSubview(descriptionLabel)
+    }
+    
+    private func setLayoutForPortrait() {
         stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 14).isActive = true
         stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -14).isActive = true
         stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
@@ -78,6 +111,24 @@ class ExpositionDetailsViewController: UIViewController {
         scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    private func setLayoutForLandscape() {
+        stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 14).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -14).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        stackView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 20).isActive = true
+        stackView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.7, constant: -28).isActive = true
+        
+        scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        scrollView.leadingAnchor.constraint(equalTo: imageView.trailingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        
+        imageView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.3).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        
     }
     
     private func setAttribute() {
